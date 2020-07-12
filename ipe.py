8c612641a3b024af27e9d400dc87ce6d819dd255
xarlan <egor21@gmail.com>
2020-07-12 23:40:41 +0300

    create ipe, requrements

---
+++ ipe.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/
+
+import click
+
+CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
+
+
+@click.group(context_settings=CONTEXT_SETTINGS)
+def cli():
+    pass
+
+@click.command()
+def initdb():
+    print("init db")
+
+
+@click.command()
+def run():
+    print("run ipe")
+
+cli.add_command(initdb)
+cli.add_command(run)
+
+
+if __name__ == '__main__':
+    cli()
