d35603d2ebb9627a00209706f84d5b6a749f9d73
alustinoff <ustnovay@gmail.com>
2020-07-19 20:14:03 +0300

    created methods for projects (CRUD)

--- src/Controllers/project_controller.py
+++ src/Controllers/project_controller.py
@@ -3,16 +3,19 @@ from src.db.entities import Project, db
 from datetime import date
 
 
-def get_all_projects():
-    user = {'username': 'Team'}
-    return flask.render_template('index.html', title='begin', user=user)
+def get_many_projects(limit=50, page=0):
+    try:
+        projects = Project.query.order_by(Project.id.desc()).offset(int(limit)*abs(int(page))).limit(int(limit)).all()
+        return flask.render_template('projects.html', title='projects', projects=projects)
+    except:
+        return "Error occured!!!"
 
 
 def create_project(req):
     try:
         req_body = req.get_json()
 
-        # Make some sanitize of data
+        # TODO: Make some sanitize of data
         name = req_body['name']
         description = req_body['description']
         date_from = date.fromtimestamp(int(req_body['date_from']))
@@ -32,14 +35,68 @@ def create_project(req):
                                   mimetype="application/json")
 
         except:
-            return "Error occured!!!"
+            return flask.Response(response='{"status": 0, "error": "Error during creating project" }',
+                                  status=500,
+                                  mimetype="application/json")
     except:
-        return "Error occured!!!"
+        return flask.Response(response='{"status": 0, "error": "Incorrect input data" }',
+                              status=500,
+                              mimetype="application/json")
 
 
-def edit_project():
-    return 'OK'
+def edit_project(req):
+    try:
+        req_body = req.get_json()
+        project = Project.query.get(int(req_body['project_id']))
+
+        project.name = req_body['name']
+        project.description = req_body['description']
+        project.date_from = date.fromtimestamp(int(req_body['date_from']))
+        project.date_to = date.fromtimestamp(int(req_body['date_to']))
+        project.host_history = int(req_body['host_history'])
+        project.retro_delete = int(req_body['retro_delete'])
+        project.scope_hosts = req_body['scope_hosts']
+
+        try:
+            db.session.commit()
+            return flask.Response(response='{"status": 1}',
+                                status=200,
+                                mimetype="application/json")
+        except:
+            return flask.Response(response='{"status": 0, "error": "Error occured during editing project" }',
+                                  status=500,
+                                  mimetype="application/json")
+    except:
+        return flask.Response(response='{"status": 0, "error": "Incorrect input data" }',
+                              status=500,
+                              mimetype="application/json")
 
 
-def delete_project():
-    return 'OK'
+
+def delete_project(req):
+    try:
+        req_body = req.get_json()
+        project_id = int(req_body['project_id'])
+        project = Project.query.get_or_404(project_id)
+
+        try:
+            db.session.delete(project)
+            db.session.commit()
+            return flask.Response(response='{"status": 1}',
+                              status=200,
+                              mimetype="application/json")
+
+        except:
+            return flask.Response(response='{"status": 0, "error": "Error during deleting project" }',
+                                  status=500,
+                                  mimetype="application/json")
+
+    except:
+        return flask.Response(response='{"status": 0, "error": "Error occured during processing input data." }',
+                              status=500,
+                              mimetype="application/json")
+
+
+def get_project(id):
+    project = Project.query.get_or_404(id)
+    return flask.render_template('project.html', title='project', project=project)
--- src/routes/routes.py
+++ src/routes/routes.py
@@ -5,10 +5,17 @@ from flask import request
 # Projects
 
 
-@app.route('/')
+@app.route('/projects/', methods=['GET'], defaults={'limit': 50, 'page': 0})
+@app.route('/projects/<limit>/', methods=['GET'], defaults={'page': 0})
+@app.route('/projects/<limit>/<page>', methods=['GET'])
 # name of function the same as name of Controller method
-def get_all_projects():
-    return project.get_all_projects()
+def get_many_projects(limit, page):
+    return project.get_many_projects(limit, page)
+
+
+@app.route('/project/<int:id>')
+def get_project(id):
+    return project.get_project(id)
 
 
 @app.route('/api/creteProject', methods=['POST'])
@@ -18,7 +25,12 @@ def create_project():
 
 @app.route('/api/editProject', methods=['POST'])
 def edit_project():
-    return project.edit_project()
+    return project.edit_project(request)
+
+
+@app.route('/api/deleteProject/', methods=['POST'])
+def delete_project():
+    return project.delete_project(request)
 
 
 # Host
